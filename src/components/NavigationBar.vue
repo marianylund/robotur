2203929eac98e3aa0553abb3162540963bbc8e99
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-02-27 16:19:11 +0100

    Clicking on logo takes you back to home page

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,27 +2,35 @@
 <template>
   <v-container>
     <v-app-bar app dark>
+      <router-link id="icon-link" to="/">
+        <v-icon class="mr-200">mdi-robot</v-icon>
+      </router-link>
 
-        <v-icon class = "mr-200">mdi-robot</v-icon>
-
-        <v-toolbar-title>Robotur</v-toolbar-title>
+      <v-toolbar-title style="cursor: pointer" @click="$router.push('/')"
+        >Robotur</v-toolbar-title
+      >
     </v-app-bar>
-
   </v-container>
 </template>
 
+<style lang="scss">
+#icon-link {
+  text-decoration: none;
+}
+</style>
+
 <script>
-  export default {
-    mounted () {
-      console.log(this.$vuetify.breakpoint)
+export default {
+  mounted() {
+    console.log(this.$vuetify.breakpoint);
+  },
+  computed: {
+    barVisibility() {
+      if (this.$vuetify.breakpoint.name == "xs") {
+        return true;
+      }
+      return false;
     },
-    computed: {
-      barVisibility () {
-          if(this.$vuetify.breakpoint.name == 'xs'){
-              return true;
-          }
-          return false;
-      },
-    },
-  }
+  },
+};
 </script>
