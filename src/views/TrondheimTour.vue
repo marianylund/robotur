c0f4b0ea75c8de42a08c09da73e55f279303ad80
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-03-16 17:36:08 +0100

    Fixed positioning of wheather box

--- src/views/TrondheimTour.vue
+++ src/views/TrondheimTour.vue
@@ -1,10 +1,19 @@
 // Page you to when you click explore on the Trondheim Tour card
 <template>
   <v-container fluid>
-    <Weather id="wheatherbox" :show="showWeather" :lat="63.4305" :lon="10.3951" />
-    <PhotoCarousel :show="showingCarousel" :mapPlace="mapPlace" v-on:button-on-photo="handleEventFormPhoto"/>
-    <Robo :text="roboText" :open="showingRobo"/>
-    <Map :show="!showingCarousel" v-on:button-on-photo="handleEventFormPhoto"/>
+    <Weather
+      id="wheatherbox"
+      :show="showWeather"
+      :lat="63.4305"
+      :lon="10.3951"
+    />
+    <PhotoCarousel
+      :show="showingCarousel"
+      :mapPlace="mapPlace"
+      v-on:button-on-photo="handleEventFormPhoto"
+    />
+    <Robo :text="roboText" :open="showingRobo" />
+    <Map :show="!showingCarousel" v-on:button-on-photo="handleEventFormPhoto" />
   </v-container>
 </template>
 
@@ -24,10 +33,15 @@ import { mapGetters } from "vuex";
     Weather,
   },
   // Get variables from the store, to be able to use them in components
-  computed: mapGetters(["roboText", "showingRobo", "showingCarousel", "mapPlace", "showWeather"]), 
+  computed: mapGetters([
+    "roboText",
+    "showingRobo",
+    "showingCarousel",
+    "mapPlace",
+    "showWeather",
+  ]),
 })
 export default class Home extends Vue {
-
   handleEventFormPhoto(button: any) {
     // Update state
     this.$store.commit("buttonOnPhotoClicked", button);
@@ -36,10 +50,10 @@ export default class Home extends Vue {
 </script>
 <style scoped>
 #wheatherbox {
-  position: absolute;
-  top: 0px;
+  position: fixed;
+  top: 55px;
   right: 0px;
   text-align: left;
-  z-index: 10;
+  z-index: 0;
 }
 </style>
